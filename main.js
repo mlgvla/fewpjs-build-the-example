f13c4edde9f7e21686beba2aac161ea90ae07421
mlgvla <mlgerardvla@gmail.com>
2021-07-11 13:43:22 -0400

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+    
+    let likeGlyphs = document.querySelectorAll('.like-glyph')
+
+    likeGlyphs.forEach(heartGlyph => {
+        heartGlyph.addEventListener('click', toggleLike )
+    });
+})
+
+function toggleLike(e) {
+    const heart = e.target
+
+    mimicServerCall()
+    .then((resp) => {
+        if (heart.classList.contains('activated-heart')) {
+            heart.classList.remove('activated-heart')
+        } else {
+            heart.setAttribute('class', 'activated-heart')
+        }
+       
+    })
+    .catch((error)=> {
+        let errorModal = document.getElementById('modal')
+        errorModal.classList.remove('hidden')
+        errorModal.innerHTML = error
+        setTimeout(() => errorModal.setAttribute('class', 'hidden'), 5000)
+    })
+   
+}
 
 
 
